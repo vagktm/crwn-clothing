d988b98a40eaa07d9d0c4cdbb1cee76a083180f6
Vag Ktm <vag_ktm@hotmail.com>
2020-12-12 23:14:49 +0200

    S05E77

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import React from 'react'
 
 function App() {
-  return <div>
-    <HomePage/>
-  </div>;
+  //console.log(props.match.params.hatid)
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/as" component={HomePage} />
+        <Route path="/hats/:hatid" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-      className="background-image"
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, history,linkUrl,match }) => {
+  //console.log(`${match.url}${linkUrl}`);
+  return (
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)} >
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+        className="background-image"
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
