2217fb1d37c46cf591365d229de8eb0b6895de81
Vag Ktm <vag_ktm@hotmail.com>
2020-12-13 16:32:00 +0200

    S06E81

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
+
 import { Switch, Route } from "react-router-dom";
-import React from 'react'
+
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   //console.log(props.match.params.hatid)
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
