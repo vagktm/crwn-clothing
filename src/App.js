58fa8eca660bce01e2329be0bd53b1b365ceb8a1
Vag Ktm <vag_ktm@hotmail.com>
2020-12-15 23:51:58 +0200

    S06E85

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 
 function App() {
   //console.log(props.match.params.hatid)
@@ -15,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
