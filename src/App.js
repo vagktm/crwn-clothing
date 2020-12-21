9a0428b63693ceed0848fa1780d9c5219bda8bda
Vag Ktm <vag_ktm@hotmail.com>
2020-12-22 00:03:51 +0200

    S07E103

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          console.log("snap", snapShot.data());
+          //console.log("snap", snapShot.data());
           this.setState(
             {
               currentUser: { id: snapShot.id, ...snapShot.data() },
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,8 +15,8 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
-  console.log(userRef);
-  console.log(snapShot);
+ // console.log(userRef);
+  //console.log(snapShot);
   // console.log(userAuth.email);
 
   if (!snapShot.exists) {
